21c037fed601e5c544e2d1b04e27c52755eeefb0
Danylo Boiko <danielboyko02@gmail.com>
2024-12-08 15:28:09 +0200

    Add data validation

--- validator/check_dataset.py
+++ validator/check_dataset.py
@@ -3,6 +3,10 @@
 import json
 import argparse
 
+from typing import Union, Literal, Optional
+
+from pydantic import BaseModel, ValidationError, field_validator
+from pydantic_core.core_schema import ValidationInfo
 from rich.console import Console
 from rich.panel import Panel
 from rich.syntax import Syntax
@@ -10,10 +14,99 @@ from rich.text import Text
 from rich.tree import Tree
 
 
-class ValidationError:
-    def __init__(self, entity_index: int, message: str) -> None:
-        self.entity_index = entity_index
+class EntrySchema(BaseModel):
+    language: str
+    country: str
+    file_name: str
+    source: str
+    license: str
+    level: str
+    category_en: str
+    category_original_lang: str
+    original_question_num: Union[int, str]
+    question: str
+    options: list[str]
+    answer: int
+    image_png: Optional[str]
+    image_information: Literal["useful", "essential"]
+    image_type: Literal["symbols", "figures", "graph", "table", "text"]
+    parallel_question_num: Optional[Union[int, str]]
+
+    @staticmethod
+    def _validate_string(value: str) -> str:
+        if len(value.strip()) == 0:
+            raise ValueError("Value cannot be empty or whitespace")
+
+        if value.startswith(" ") or value.endswith(" "):
+            raise ValueError("Value cannot have leading or trailing spaces")
+
+        return value
+
+    @staticmethod
+    def _validate_uniqueness(values: list) -> list:
+        if len(set(values)) != len(values):
+            raise ValueError("All values must be unique")
+
+        return values
+
+    @staticmethod
+    def _validate_length(values: list, expected_length) -> list:
+        length = len(values)
+
+        if length != expected_length:
+            raise ValueError(f"Expected {expected_length} values, but got {length}")
+
+        return values
+
+    @field_validator("language")
+    def validate_language(cls, language: str, config: ValidationInfo) -> str:
+        expected_language = config.context.get("language")
+
+        if language != expected_language:
+            raise ValueError(f"Expected '{expected_language}', but got '{language}'")
+
+        return cls._validate_string(language)
+
+    @field_validator("options")
+    def validate_options(cls, options: list[str]) -> list[str]:
+        for option in options:
+            cls._validate_string(option)
+
+        cls._validate_uniqueness(options)
+
+        return cls._validate_length(options, 4)
+
+    @field_validator("answer")
+    def validate_answer(cls, answer: int, config: ValidationInfo) -> int:
+        options_count = len(config.data.get("options", []))
+
+        if options_count != 0 and answer not in range(options_count):
+            raise ValueError(f"Expected value from 0 to {options_count - 1}, but got {answer}")
+
+        return answer
+
+    @field_validator(
+        "country", "file_name", "source", "license", "level", "category_en", "category_original_lang",
+        "original_question_num", "question", "image_png", "parallel_question_num"
+    )
+    def validate_string_fields(cls, value: Optional[str]) -> Optional[str]:
+        return cls._validate_string(value) if isinstance(value, str) else value
+
+    class Config:
+        extra = "forbid"
+
+
+class EntryError:
+    def __init__(self, index: int, location: tuple | None, message: str) -> None:
+        self.index = index
+        self.location = location
         self.message = message
+
+    def __str__(self) -> str:
+        if self.location:
+            return f"Location: {str(self.location).strip("(,)")}, error: {self.message.lower()}"
+
+        return self.message
 
 
 class DatasetValidator:
@@ -22,10 +115,10 @@ class DatasetValidator:
         self.json_entries: list[dict] = []
         self.language_code: str = language_code.lower()
         self.console: Console = Console()
-        self.errors: list[ValidationError] = []
+        self.errors: list[EntryError] = []
 
     def validate(self) -> None:
-        self.console.print("Starting validation...", style="bold green")
+        self.console.print("Starting validation...", style="green")
         self.console.print(f"JSON file: {self.json_file}", style="cyan")
         self.console.print(f"Language code: {self.language_code}", style="cyan")
 
@@ -47,13 +140,21 @@ class DatasetValidator:
                 self.json_entries = entries
 
             return True
-        except Exception as error:
-            self.console.print(f"Error loading file {self.json_file}: {error}", style="red")
+        except Exception as e:
+            self.console.print(f"Error loading file {self.json_file}: {e}", style="red")
 
         return False
 
     def _validate_entries(self) -> None:
-        pass
+        for index, entry in enumerate(self.json_entries):
+            try:
+                EntrySchema.model_validate(entry, context={
+                    "language": self.language_code
+                })
+            except ValidationError as e:
+                self.errors.extend([
+                    EntryError(index, error.get("loc", None), error.get("msg")) for error in e.errors()
+                ])
 
     def _print_validation_report(self) -> None:
         if len(self.errors) == 0:
@@ -64,21 +165,21 @@ class DatasetValidator:
         for error in self.errors:
             self.console.print(Panel(self._create_error_tree(error), expand=False, border_style="red"))
 
-    def _create_error_tree(self, error: ValidationError) -> Tree:
-        entry = self.json_entries[error.entity_index]
+    def _create_error_tree(self, error: EntryError) -> Tree:
+        entry = self.json_entries[error.index]
 
-        tree = Tree(f"Error in entry with index {error.entity_index}", style="red")
-        tree.add(Text(error.message, style="yellow"))
+        tree = Tree(f"Error in entry with index {error.index}", style="red")
+        tree.add(Text(str(error), style="yellow"))
 
         question_node = tree.add("Question")
-        question_node.add(Syntax(entry.get("question", "[N/A]"), "text", word_wrap=True))
+        question_node.add(Syntax(entry.get("question", "N/A"), "text", word_wrap=True))
 
         options_node = tree.add("Options")
         for option_num, option_value in enumerate(entry.get("options", []), 1):
             options_node.add(f"{option_num}. {option_value}")
 
         answer_node = tree.add("Answer")
-        answer_node.add(str(entry.get("answer", "[N/A]")))
+        answer_node.add(str(entry.get("answer", "N/A")))
 
         return tree
 
